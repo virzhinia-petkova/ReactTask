45c38871f8208dc60fa9410a2202c3f66560e80c
virzhinia-petkova <VirzhiniaPetkova@coherentsolutions.com>
2021-03-25 11:49:33 +0200

    visualize the current weather from the api

---
+++ src/components/SearchList.jsx
@@ -0,0 +1,28 @@
+import { useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+import Link from './Link';
+
+const SearchList = ({ searchResults, onClick }) => {
+  const memoizedSearch = useMemo(
+    () =>
+    // sort by country and then by city
+      [...searchResults].sort(
+        (a, b) => a.country.localeCompare(b.country) || a.name.localeCompare(b.name)
+      ),
+    [searchResults]
+  );
+  return searchResults.length > 0 ? (
+    memoizedSearch.map(result => (
+      <Link place={`${result.name}, ${result.country}`} key={result.id} onClick={onClick} />
+    ))
+  ) : (
+    <p>Ooops, looks like we couldn't find anything</p>
+  );
+};
+SearchList.propTypes = {
+  currentSearch: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+export default SearchList;
