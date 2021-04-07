6a8c03c0ee874daeddc47af3ff54d86d6167e099
virzhinia-petkova <VirzhiniaPetkova@coherentsolutions.com>
2021-04-07 14:23:51 +0300

    format code

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,43 +1,37 @@
 import PropTypes from 'prop-types';
 import { WEATHER_SYMBOL_BASE_URL } from '../common/constants';
 
-const Current = ({
-  cityName,
-  current,
-}) => {
+const Current = ({ cityName, current }) => {
   return (
-          <section className="weather__current">
-            <>
-              <h1>{cityName}</h1>
-              <img
-                src={`${WEATHER_SYMBOL_BASE_URL}/${current.symbol}.png`}
-                alt={current.symbolPhrase}
-              />
-              <h2>{current.temperature}°C</h2>
-              <hr />
-              <p>
-                Cloudiness: <b>{current.cloudiness}%</b>
-              </p>
-              <p>
-                Visibility: <b>{current.visibility} m</b>
-              </p>
-              <p>
-                Pressure: <b>{current.pressure} hPa</b>
-              </p>
-              <p>
-                Wind speed: <b>{current.windSpeed} m/s</b>
-              </p>
-              <p>
-                Wind direction: <b>{current.windDirString}</b>
-              </p>
-            </>
-          </section>
+    <section className="weather__current">
+      <>
+        <h1>{cityName}</h1>
+        <img src={`${WEATHER_SYMBOL_BASE_URL}/${current.symbol}.png`} alt={current.symbolPhrase} />
+        <h2>{current.temperature}°C</h2>
+        <hr />
+        <p>
+          Cloudiness: <b>{current.cloudiness}%</b>
+        </p>
+        <p>
+          Visibility: <b>{current.visibility} m</b>
+        </p>
+        <p>
+          Pressure: <b>{current.pressure} hPa</b>
+        </p>
+        <p>
+          Wind speed: <b>{current.windSpeed} m/s</b>
+        </p>
+        <p>
+          Wind direction: <b>{current.windDirString}</b>
+        </p>
+      </>
+    </section>
   );
 };
 
 Current.propTypes = {
   cityName: PropTypes.string.isRequired,
-  current: PropTypes.object.isRequired,
+  current: PropTypes.object.isRequired
 };
 
 export default Current;
--- src/components/Daily.jsx
+++ src/components/Daily.jsx
@@ -2,30 +2,28 @@ import PropTypes from 'prop-types';
 
 import { WEATHER_SYMBOL_BASE_URL } from '../common/constants';
 
-const Daily = ({
-  daily
-}) => (
-          <section className="weather__days">
-            {daily.map(day => (
-              <div className="weather__days__daily" key={day.date}>
-                <h3>{new Date(day.date).toDateString().slice(0, 3)}</h3>
-                <img src={`${WEATHER_SYMBOL_BASE_URL}/${day.symbol}.png`} alt={day.symbolPhrase} />
-                <p>
-                  <b>{day.minTemp}°C</b> | <b>{day.maxTemp}°C</b>
-                </p>
-                <p>
-                  Rain: <b>{day.precipProb}%</b>
-                </p>
-                <p>
-                  Wind: <b>{day.maxWindSpeed} m/s</b>
-                </p>
-              </div>
-            ))}
-          </section>
-  );
+const Daily = ({ daily }) => (
+  <section className="weather__days">
+    {daily.map(day => (
+      <div className="weather__days__daily" key={day.date}>
+        <h3>{new Date(day.date).toDateString().slice(0, 3)}</h3>
+        <img src={`${WEATHER_SYMBOL_BASE_URL}/${day.symbol}.png`} alt={day.symbolPhrase} />
+        <p>
+          <b>{day.minTemp}°C</b> | <b>{day.maxTemp}°C</b>
+        </p>
+        <p>
+          Rain: <b>{day.precipProb}%</b>
+        </p>
+        <p>
+          Wind: <b>{day.maxWindSpeed} m/s</b>
+        </p>
+      </div>
+    ))}
+  </section>
+);
 
-  Daily.propTypes = {
-  daily: PropTypes.arrayOf(PropTypes.object).isRequired,
+Daily.propTypes = {
+  daily: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default Daily;
